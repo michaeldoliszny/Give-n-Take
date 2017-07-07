91505953b401451fd665fc0bfac6f411bacbe554
Evan Doliszny <Overlord441@Evans-MacBook-Pro.local>
2017-07-07 13:53:40 -0500

    Initial Commit

---
+++ Lend n' Spend/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lend n' Spend
+//
+//  Created by Evan Doliszny on 7/7/17.
+//  Copyright © 2017 Michael Doliszny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
